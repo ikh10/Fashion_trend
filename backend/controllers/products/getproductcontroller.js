deec9239dd40668c4565506f634fff699f5adcd4
shikha mahar <shikhamahar27@gmail.com>
2024-08-08 10:29:58 +0530

    updated

--- backend/controllers/products/getproductcontroller.js
+++ backend/controllers/products/getproductcontroller.js
@@ -5,12 +5,14 @@ const ProductCollection = require("../../models/ProductSchema");
 // })
 const getproductcontroller = async(req,res) => {
     try {
-        const {category, name, subcategory} = req.params;
+        const {category, name, sub_category, id} = req.params;
         let products;
         if(category)
         {
-            const searchcategory = category.toLowerCase();
-            products = await ProductCollection.find({category: {$regex: new RegExp(searchcategory,"i")},});
+            const searchCategory = category.toLowerCase();
+            products = await ProductCollection.find({
+                category: {$regex: new RegExp(searchCategory,"i")}
+            });
         }
         else if (name) {
             const searchname = name.toLowerCase();
@@ -18,27 +20,50 @@ const getproductcontroller = async(req,res) => {
                 name: {$regex: new RegExp(searchname, "i")},
             });
         }
-        else if (subcategory) {
-            const searchsubcategory = subcategory.toLowerCase();
-            product = await ProductCollection.find({
-                subcategory: {$regex: new RegExp(searchsubcategory,"i")},
+        else if (sub_category) {
+            const searchsubcategory = sub_category.toLowerCase();
+            products = await ProductCollection.find({
+                sub_category: {$regex: new RegExp(searchsubcategory,"i")},
             });
+        }
+        else if (id) {
+            products = await ProductCollection.find({
+                _id: id,
+            });
+        }
+        else if (req.path.includes("/random")) {
+            products = await ProductCollection.aggregate([
+                {
+                    $sample:{
+                        size:9,
+                    }
+                }
+            ],);
+        }
+        else if (req.path.includes("/top-rated")) {
+            products = await ProductCollection.find().sort({rating:-1}).limit(9);
+        }
+        else if (req.path.includes("/lowToHigh")) {
+            products = await ProductCollection.find().sort({new_price:1}).limit(9);
+        }
+        else if (req.path.includes("/highToLow")) {
+            products = await ProductCollection.find().sort({new_price:-1}).limit(9);
         }
         else
         {
-        const product = await ProductCollection.find();
-        
+        products = await ProductCollection.find();
         console.log(`product fetched successfully `)
         }
-        if(products || products.length == 0)
-            return res.status(404).send({message: "Product not found"});
+        if(!products || products.length == 0){
+            return res.status(404).json({message: "Product not found"});
+        }     
         res.status(200).send(products);
     } 
     catch (error) {
         res.status(504).send({
             message: "Error fetching products",
-        })
-        console.log(`Error Occured :$(error)`);
+        });
+        console.log(`Error Occured :$(error1)`);
     }
 }
-module.exports = getproductcontroller
+module.exports = getproductcontroller;
--- backend/routes/productroute.js
+++ backend/routes/productroute.js
@@ -5,5 +5,10 @@ router.get("/", getproductcontroller);
 router.get("/category/:category", getproductcontroller);
 router.get("/name/:name", getproductcontroller);
 router.get("/subcategory/:subcategory", getproductcontroller);
+router.get("/id/:id",getproductcontroller);
+router.get("/random",getproductcontroller);
+router.get("/top-rated", getproductcontroller);
+router.get("/lowToHigh",getproductcontroller);
+router.get("/highToLow",getproductcontroller);
 
 module.exports = router;
